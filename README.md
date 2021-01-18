0dbe69b09a9396ab578105670e294a61472da973
Filipe <filipefbn@gmail.com>
2021-01-18 10:39:29 -0300

    Index persistence

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# elo7
+# Elo7
 Repositório referente ao teste prático para o processo seletivo Elo7
+
+
+## Considerações
+* Foi abstraída a noção de armazenamento e acesso eficiente dos documentos. Para os propósitos desse teste, restringimos a atenção apenas à recuperação/ranqueamento do id dos produtos. Assim, o índice construído "não se preocupa" em como as informações adicionais dos documentos são acessadas.
--- search.py
+++ search.py
@@ -13,17 +13,19 @@ class InvertedIndex:
     """
     def __init__(self, data_path, normalization=True, accents=False, stemming=True,
                  remove_stopwords=True, force_reindex=False):
-        print('Building index...')
-        # Text processing settings
         self.normalization = normalization
         self.accents = accents
         self.stemming = stemming
         self.remove_stopwords = remove_stopwords
         self.dictionary = defaultdict(list)
         
-        # Indexing
-        with open(path, 'r', encoding='utf-8') as f:
-            next(f) # Skip header
+        if not force_reindex:
+            print('Loading index...')
+            self.load_index()
+        if not bool(self.dictionary): # If index is still empty (force_reindex or failed load)
+            print('Building index...')
+            with open(path, 'r', encoding='utf-8') as f:
+                next(f) # Skip header
             for line in f.readlines():
                 entry = line.rstrip().split(',')
                 product_id = entry[0]
@@ -34,16 +36,7 @@ class InvertedIndex:
                 
     def index_product(self, product_id, title):
         tokens = nltk.word_tokenize(title, language='portuguese')
-        if self.normalization:
-            tokens = [token.lower() for token in tokens if token.isalpha()]
-        if self.remove_stopwords:
-            stopwords = set(nltk.corpus.stopwords.words('portuguese'))
-            tokens = [token.lower() for token in tokens if token not in (stopwords)]
-        if not self.accents:
-            tokens = remove_accents(tokens)
-        if self.stemming:
-            stemmer = nltk.stem.RSLPStemmer()
-            tokens = [stemmer.stem(token) for token in tokens]
+        tokens = text_processing(tokens, self.normalization, self.accents, self.stemming, self.remove_stopwords)
 
         for token in tokens:                 
             product_list = [product[0] for product in self.dictionary[token]]
@@ -57,14 +50,63 @@ class InvertedIndex:
                 self.dictionary[token][lookup_index][1] += 1
                 
         
-    def load_index(self, path):
-        pass
+    def load_index(self, path='products.ind'):
+        try:
+            with open(path, 'rb') as f:
+                data = pickle.load(f)
+                self.dictionary = data['index']
+                self.normalization = data['normalization']
+                self.accents = data['accents']
+                self.stemming = data['stemming']
+                self.remove_stopwords = data['remove_stopwords']
+        except:
+            print('Could not load index.')
     
-    def save_index(self, path):
+    def save_index(self, path='products.ind'):
+        try:
+            "Settings file + index"
+            save_dict = {'index': self.dictionary,
+                         'normalization': self.normalization,
+                         'accents': self.accents,
+                         'stemming': self.stemming,
+                         'remove_stopwords': self.remove_stopwords}
+
+            with open(path, 'wb') as f:
+                pickle.dump(save_dict, f)
+        except:
+            print('Could not save index.')
+
+
+class BM25Ranker:
+    def __init__(self, parameters):
         pass
-                
+
+    def rank(self, query):
+        pass
+
+class Search:
+    def __init__(self, inverted_index, ranker):
+        self.inverted_index = inverted_index
+        self.ranker = ranker
+
+
 # Helper functions                
-                
+
+def text_processing(tokens,  normalization, accents, stemming, 
+                    remove_stopwords):
+    "Performs common text preprocessing operations."    
+    if normalization:
+            tokens = [token.lower() for token in tokens if token.isalpha()]
+    if remove_stopwords:
+        stopwords = set(nltk.corpus.stopwords.words('portuguese'))
+        tokens = [token.lower() for token in tokens if token not in (stopwords)]
+    if not accents:
+        tokens = remove_accents(tokens)
+    if stemming:
+        stemmer = nltk.stem.RSLPStemmer()
+        tokens = [stemmer.stem(token) for token in tokens]
+    return tokens
+
 def remove_accents(text):
     """
     Removes Portuguese accents from input text.
